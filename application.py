7453c8f0f58467f1ec1ca2e123d833f7b267387e
Nigel Babu <nigelbabu@gmail.com>
2010-11-27 10:28:21 +0530

    Index page looks lovely now

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 import ConfigParser, os
-from flask import render_template, Flask
+from flask import render_template, Flask, url_for
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,6 +7,13 @@ def index():
     config = ConfigParser.ConfigParser()
     config.read('gitosis.conf')
     return render_template('index.html', config=config)
+
+@app.route('/group/<groupname>')
+def showgroup(groupname):
+    config = ConfigParser.ConfigParser()
+    config.read('gitosis.conf')
+    section = 'group ' + groupname
+    return render_template('group.html', config=config,  group=groupname, section=section)
 
 if __name__ == '__main__':
     app.run(debug=True)
