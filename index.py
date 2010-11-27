1b433e5c82c140c44cf7bd999418112f6a3dd8ec
Nigel Babu <nigelbabu@gmail.com>
2010-11-27 07:42:08 +0530

    Created a template file

--- index.py
+++
@@ -1,9 +0,0 @@
-import ConfigParser, os
-
-config = ConfigParser.ConfigParser()
-config.read('gitosis.conf')
-
-for section in config.sections():
-    print section
-    for option in config.options(section):
-        print " ", option, "=", config.get(section, option)
