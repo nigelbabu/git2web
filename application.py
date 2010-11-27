f62ff564c0e23b01920fd1fab89cb2a16e138b31
Nigel Babu <nigelbabu@gmail.com>
2010-11-27 11:23:38 +0530

    Wrote function to parse key dir

--- application.py
+++ application.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 def index():
     config = ConfigParser.ConfigParser()
     config.read(path_to_conf())
+    list_of_members()
     return render_template('index.html', config=config)
 
 @app.route('/group/<groupname>')
@@ -17,10 +18,18 @@ def showgroup(groupname):
     section = 'group ' + groupname
     return render_template('group.html', config=config,  group=groupname, section=section)
 
-def path_to_conf():
+def path_to_gitosis():
     config = ConfigParser.ConfigParser()
     config.read('git2web.cfg')
-    return config.get('git2web', 'gitosis-path') + 'gitosis.conf'
+    return config.get('git2web', 'gitosis-path')
+
+def path_to_conf():
+    return path_to_gitosis() + 'gitosis.conf'
+
+def list_of_members():
+    path = path_to_gitosis()
+    path_to_key = os.path.join(path, 'keydir')
+    return map(lambda x: x.split('.pub')[0], os.listdir(path_to_key))
 
 if __name__ == '__main__':
     app.run(debug=True)
