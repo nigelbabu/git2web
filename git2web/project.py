8a168469853e6f19e3cadb510ac29583bd5e21b2
Nigel Babu <nigelbabu@gmail.com>
2010-12-11 23:10:26 +0530

    Deleting a person works

--- git2web/project.py
+++ git2web/project.py
@@ -1,7 +1,8 @@
 from git2web import app
 from git2web.functions import path_to_conf
 from configobj import ConfigObj
-from flask import redirect, g, session, url_for, render_template, flash
+from flask import session, redirect, g, url_for, render_template, \
+    flash
      
 # index, project list
 @app.route('/')
@@ -10,6 +11,20 @@ def index():
         return redirect(url_for('login'))
     config = ConfigObj(path_to_conf())
     return render_template('index.html', config=config)
+
+#add person to project
+@app.route('/group/<groupname>/delete/<person>')
+def remove_project_person(groupname, person):
+    config = ConfigObj(path_to_conf())
+    section = 'group ' + groupname
+    members = config[section]['members'].split()
+    if person in members:
+        members.remove(person)
+    config[section]['members'] = ' '.join(members)
+    try
+    config.write()
+    flash('Deleted person')
+    return redirect(url_for('showgroup', groupname=groupname))
 
 # individual groups
 @app.route('/group/<groupname>')
@@ -20,6 +35,6 @@ def showgroup(groupname):
     section = 'group ' + groupname
     if section not in config:
         flash('Group not found')
-        return redirect(url_for('index')
+        return redirect(url_for('index'))
     else:
         return render_template('group.html', config=config,  group=groupname, section=section)
